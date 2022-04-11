1a2b92723dc8b23df471f8a6ad574aeb56ea89fe
lad <85745878+LADzhub@users.noreply.github.com>
2022-04-11 21:48:01 +0530

    Update stickers.py

--- ShuKurenaiXRoBot/modules/stickers.py
+++ ShuKurenaiXRoBot/modules/stickers.py
@@ -574,7 +574,7 @@ def delsticker(update, context):
             "Please reply to sticker message to del sticker"
         )
 
-__mod_name__ = "Stickers"
+__mod_name__ = "𝙨𝙩𝙞𝙘𝙠𝙚𝙧𝙨"
 
 __help__ = """
 *Help menu for stickers tools*
